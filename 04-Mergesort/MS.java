bc1d95b1b650091d1b559803dda0d3fe2dd7c739
jelliott37 <jessee8991@gmail.com>
2015-02-25 09:27:17 -0500

    stuff

--- 04-Mergesort/MS.java
+++ 04-Mergesort/MS.java
@@ -1,7 +1,38 @@
 public class MS{
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
-	
+	ArrayList<Integer> c = new ArrayList<Integer>();
+	while(b.size() > 0 || a.size() > 0){
+	    if(b.size()==0){
+		c.add(a.remove(0));
+	    } else if (a.size() == 0){
+	        c.add(b.remove(0));
+	    } else if (a.get(0) > b.get(0)){
+		c.add(b.remove(0));
+	    } else {
+		c.add(a.remove(0));
+	    }
+	}
+	return c;
     }
+
+    public int[] merge(int[] a, int[] b){
+	int[] c = new int[a.length+b.length];
+	int bit=0;
+	int ait=0;
+	int cit=0;
+	while(bit<b.length||ait<a.length){
+	    if(bit == b.length){
+		c[cit]=a[ait];
+		cit++;
+		ait++;
+	    } else if (ait == a.length){
+		c[cit]=b[bit];
+		cit++;
+		bit++;
+	    }
+	}
+    }
+
     public static void main(String[] args){
 	MS m = new MS();
 	System.out.println(m.merge());
