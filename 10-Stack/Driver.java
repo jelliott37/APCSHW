8e2c271ada052c1575fcd778416235aa19c8154b
jelliott37 <jessee8991@gmail.com>
2015-03-24 09:00:40 -0400

    finished stack

---
+++ 10-Stack/Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+    public static void main(String[] args){
+	myStack<String> ms = new myStack<String>("blah");
+	System.out.println(ms.top());//blah
+	System.out.println(ms.pop());//blah
+	System.out.println(ms.pop());//null
+	System.out.println(ms.empty());//true
+	ms.push("BLAH");//nothing
+	System.out.println(ms.top());//BLAH
+	
+    }
+}
