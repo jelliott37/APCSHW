bb4ba6e09c3302360386e1ed25d6a54923521057
jelliott37 <jessee8991@gmail.com>
2015-03-24 09:22:40 -0400

    stuff

--- 10-Stack/Driver.java
+++ 10-Stack/Driver.java
@@ -1,5 +1,6 @@
 public class Driver{
     public static void main(String[] args){
+	/*
 	myStack<String> ms = new myStack<String>("blah");
 	System.out.println(ms.top());//blah
 	System.out.println(ms.pop());//blah
@@ -7,6 +8,14 @@ public class Driver{
 	System.out.println(ms.empty());//true
 	ms.push("BLAH");//nothing
 	System.out.println(ms.top());//BLAH
+	*/
 	
+	myStackArray<String> ms = new myStackArray<String>("blah");
+	System.out.println(ms.top());//blah
+	System.out.println(ms.pop());//blah
+	System.out.println(ms.pop());//null
+	System.out.println(ms.empty());//true
+	ms.push("BLAH");//nothing
+	System.out.println(ms.top());//BLAH
     }
 }
