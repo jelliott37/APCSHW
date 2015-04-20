f8b46cd79e37ba52076be2adffc1fae5f8877220
jelliott37 <jessee8991@gmail.com>
2015-04-19 20:17:31 -0400

    stuff

---
+++ 13-BetterSearch/Frontier.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Frontier {
+    public LinkedList<Node> l = new LinkedList<Node>();
+
+    public void add(Node n){
+	l.add(n);
+    }
+
+    public Node remove() {
+	return l.remove(0);
+    }
+
+    public boolean isEmpty(){
+	return l.isEmpty();
+    }
+    public void addInOrder(Node n){
+	int index = 0;
+	for (Node temp : l){
+	    if(temp.getPriority()>n.getPriority){
+		l.add(temp,index);
+		break;
+	    }
+	    index++;
+	}
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
