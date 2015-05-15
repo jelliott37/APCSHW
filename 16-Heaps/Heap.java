1522aaf925460f5c64a57dde1e79fb67af691fec
jelliott37 <jessee8991@gmail.com>
2015-05-15 09:26:10 -0400

    stuff

--- 16-Heaps/Heap.java
+++ 16-Heaps/Heap.java
@@ -1,6 +1,78 @@
-import java.util.*;
-import java.io.*;
-public class Heap{
-    ArrayList<Integer> data = new ArrayList<Integer>;
-    
+public class Heap{ //min heap
+    private int[] data;
+    private int last = 0;
+    public Heap(int n){ // n is of the form 2^x - 1
+	data = new int[n];
+    }
+    public Heap(int[] n){ // n has length 2^x - 1
+	data = new int[n.length];
+	for(int i : n){
+	    insert(i);
+	} 
+    }
+    public void siftup(int n){
+	if(n == 0){
+	    return ;
+	} else if(data[n] < data[(n + 1) / 2]){
+	    int ph = data[n];
+	    data[n] = data[(n+1) / 2];
+	    data[(n+1) / 2] = ph;
+	    siftup((n+1) / 2);
+	} 
+	return ;
+    }
+    public void pushdown(int n){
+	if(n > (data.length+1) / 2){
+	    return ;
+	} else if(data[n] > data[2 * n + 1]){
+	    if(data[2 * n + 1] < data[2 * n + 2]){
+		int ph = data[n];
+		data[n] = data[2 * n + 1];
+		data[2 * n + 1] = ph;
+		pushdown(2 * n + 1);
+	    } else {
+		int ph = data[n];
+		data[n] = data[2 * n + 2];
+		data[2 * n + 2] = ph;
+		pushdown(2 * n + 2);
+	    }
+	} else if(data[n] > data[2 * n + 2]){
+	    int ph = data[n];
+	    data[n] = data[2 * n + 2];
+	    data[2 * n + 2] = ph;
+	    pushdown(2 * n + 2);
+	}
+	return ;
+    }
+    public void insert(int n){
+	if (last == 0) {
+	    data[0] = n;
+	} else if (last < data.length) {
+	    data[last] = n;
+	    siftup(last);
+	} else {
+	    last--;
+	}
+	last++;
+    }
+    public int deleteMin(){
+	if(last != 0){
+	    int ph = data[0];
+	    data[0] = data[last];
+	    data[last] = ph;
+	    pushdown(0);
+	    last--;
+	    return ph;
+	}
+	return Integer.MAX_VALUE;
+    }
+    public int[] sort(){
+	int[] heap = data;
+	while(last > 0){
+	    deleteMin();
+	}
+	int[] sorted = data;
+	data = heap;
+	return sorted;
+    }    
 }
